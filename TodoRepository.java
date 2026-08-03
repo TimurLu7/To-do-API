a8705a1f9617cf32cfcd760eb214dc7d6bd7be00
Timur <timurlu7@mail.ru>
2026-08-03 23:22:27 +0300

    repo

---
+++ TodoRepository.java
@@ -0,0 +1,14 @@
+package com.example.todoapi.repository;
+
+import com.example.todoapi.model.User;
+import com.example.todoapi.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+    List<Todo> findByUser(User user);
+    List<Todo> findByUserAndCompleted(User user, boolean completed);
+}
